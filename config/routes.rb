b03054dc7f4c7f2b9eb4824b00659de7b52670e9
Chris Dopuch <chris.dopuch@gmail.com>
2014-07-19 17:50:37 -0500

    added route for checkin

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   
   root to: "groups#index"
 
-
+  match 'checkIn' => 'events#checkIn', :as => 'checkIn', via: [:get, :post]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
