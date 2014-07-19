1fe153db2ce6d38ce93bf26a6691ebfdbf80ca1d
Chris Dopuch <chris.dopuch@gmail.com>
2014-07-19 16:50:39 -0500

    added view stuff for new locations fields in event

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140719180726) do
+ActiveRecord::Schema.define(version: 20140719213308) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 20140719180726) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "group_id"
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "groups", force: true do |t|
